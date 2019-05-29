fc1b0b5a75eb3a264c5024d244794a19ceccd2f4
Cristian Martin Sguazzin <34490158+NeXuZZ-SCM@users.noreply.github.com>
2019-05-29 18:07:33 -0300

    Add files via upload

---
+++ ClienteApiRestFul0/ClienteApiRestFul0/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ClienteApiRestFul0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_Personas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Personas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_Personas
+            // 
+            this.dgv_Personas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Personas.Location = new System.Drawing.Point(12, 139);
+            this.dgv_Personas.Name = "dgv_Personas";
+            this.dgv_Personas.Size = new System.Drawing.Size(339, 150);
+            this.dgv_Personas.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 319);
+            this.Controls.Add(this.dgv_Personas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Personas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_Personas;
+    }
+}
+
---
+++ ClienteApiRestFul0/ClienteApiRestFul0/Form1.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteApiRestFul0
+{
+    public partial class Form1 : Form
+    {
+        static HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+            MostrarTabla();
+
+        }
+
+        private void MostrarTabla()
+        {
+            string url = "http://localhost:53747/ModeloPersona";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+            dgv_Personas.DataSource = m;
+
+        }
+
+    }
+}
